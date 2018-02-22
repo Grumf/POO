d8906a5def61a364ed9e487814249b8f7853509e
Grumf <Grumf@users.noreply.github.com>
2018-02-22 15:28:12 +0100

    initial commit

---
+++ classGPS.php
@@ -0,0 +1,59 @@
+<?php
+
+class GPS {
+
+	public $marque;
+	public $adresse;
+	public $position;
+	public $onAPasLesThunes = false;
+	public $bouchons = false;
+
+	public function definirUnTrajet($depart, $arrivee) {
+		return "Part de ".$depart." pour arriver à ".$arrivee.".<br>";
+	}
+
+	public function enregistreUneAdresse($adresse) {
+		return "Enregistre : ".$adresse." dans le GPS. <br>";
+	}
+
+	public function afficherLaCarte($ici) {
+		return "Me montre la carte et me positionne ".$ici." !<br>";
+	}
+
+	public function eviterLesPeages($onAPasLesThunes) {
+		if( $onAPasLesThunes == true){
+				return "On evite les péages !<br>";
+			} else {
+				return "On paye !<br>";
+			}
+	}
+
+	public function eviterLesBouchons($bouchons) {
+		if( $bouchons == true){
+				return "On fais un détour !<br>";
+			} else {
+				return "Ok, y'a pas de bouchons !<br>";
+			}
+	}
+
+	public function avertirPause($tempsTrajet) {
+		if( $tempsTrajet > 7200000){
+				return "Il faudra faire une pause.<br>";
+			}
+	}
+
+}
+
+
+$ParisLyon = new GPS();
+$ParisLyon->marque = 'TOMTOM';
+$position = $ParisLyon->position = '44 rue de chez ta mère 75001 PARIS';
+$arrivee = $ParisLyon->adresse = '57 avenue du bas 69000 LYON';
+$trajetParisLyon = $ParisLyon->tempsTrajet = 18000000;
+
+echo $ParisLyon->definirUnTrajet($position,$arrivee);
+echo $ParisLyon->enregistreUneAdresse($arrivee);
+echo $ParisLyon->afficherLaCarte($position);
+echo $ParisLyon->eviterLesPeages(true);
+echo $ParisLyon->eviterLesBouchons(false);
+echo $ParisLyon->avertirPause($trajetParisLyon);
